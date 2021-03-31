e2bb7bb86879111f4777add5edac3c790faae568
Sejarr <filippov.mikhail.andreevich@gmail.com>
2021-03-31 14:41:21 +0300

    Improved core, code correction by PEP-8

--- module_0/game_core.py
+++ module_0/game_core.py
@@ -2,33 +2,36 @@ import math
 import numpy as np
 
 
-def game_core(number):
+def game_core_impl(number):
     """Implementation of bisection method"""
     count = 0   # Steps counter
-    # Range boundaries. Values are taken from task definition
-    a = 1
-    b = 100
+    # Range boundaries. Values are taken from task definition (constant)
+    BOTTOM_BOUND = 1
+    UPPER_BOUND = 101
     while True:
         count += 1
         # Range center
-        i = math.floor((b - a) / 2)
+        center = math.floor((UPPER_BOUND - BOTTOM_BOUND) / 2)
         # Target value for current step
-        r = a + i
-        if r == number:
+        current_value = BOTTOM_BOUND + center
+        if current_value == number:
             break
-        if r < number:
-            a += i
+        if current_value < number:
+            BOTTOM_BOUND += center
         else:
-            b -= i
+            UPPER_BOUND -= center
     return count
 
 
 # This function is taken from task definition
 def score_game(game_core):
+    # Range boundaries. Values are taken from task definition (constant)
+    BOTTOM_BOUND = 1
+    UPPER_BOUND = 101
     """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=1000)
+    random_array = np.random.randint(BOTTOM_BOUND, UPPER_BOUND, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
@@ -36,4 +39,4 @@ def score_game(game_core):
     return score
 
 
-score_game(game_core)
+score_game(game_core_impl)
